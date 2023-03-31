4dc27e9dbdd87cd1d590634ef53b5a1cbe309ff7
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-31 15:07:55 +0300

    update Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,17 @@ import throttle from 'lodash.throttle';
 
 const player = new Player(document.querySelector('#vimeo-player'));
 
+savedCurrentTime();
+
 player.on('timeupdate', throttle(onPlayClick, 1000));
 
 function onPlayClick(e) {
   localStorage.setItem('videoplayer-current-time', e.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+function savedCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+}
