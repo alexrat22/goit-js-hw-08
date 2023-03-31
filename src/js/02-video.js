8068ea2623db19cb51302f72b0c0cfd3ee1ee41c
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-31 11:54:16 +0300

    done Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player(document.querySelector('#vimeo-player'));
+
+player.on('timeupdate', throttle(onPlayClick, 1000));
+
+function onPlayClick(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
